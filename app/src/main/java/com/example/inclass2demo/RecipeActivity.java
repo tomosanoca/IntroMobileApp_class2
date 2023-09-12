0dee4ae8ea4104f9b5b0aaad5e66de421d520766
Sano <sanot@student.douglascollege.ca>
2023-09-12 14:29:55 -0700

    Initial commit

---
+++ app/src/main/java/com/example/inclass2demo/RecipeActivity.java
@@ -0,0 +1,14 @@
+package com.example.inclass2demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+    }
+}
